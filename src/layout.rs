321af3788a6002de7e19959be167f7cb6c2e1576
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2022-01-22 23:41:38 -0800

    Added F1-F12 under number keys and copy and paste

--- src/layout.rs
+++ src/layout.rs
@@ -13,10 +13,17 @@ macro_rules! s {
 #[rustfmt::skip]
 pub static LAYERS: keyberon::layout::Layers = &[
     &[
-        &[k(No), k(Escape),   k(Kb1),  k(Kb2),  k(Kb3),   k(Kb4),   k(Kb5),   k(Kb6),   k(Kb7),   k(Kb8),   k(Kb9),   k(Kb0),    k(Minus),    k(Equal),    k(BSpace), k(Application)],
-        &[k(LGui), k(Tab),      k(Q),    k(W),    k(E),     k(R),     k(T),     k(Y),     k(U),     k(I),     k(O),     k(P),      k(LBracket), k(RBracket), k(Bslash), k(Delete)],
-        &[k(Mute), k(CapsLock), k(A),    k(S),    k(D),     k(F),     k(G),     k(H),     k(J),     k(K),     k(L),     k(SColon), k(Quote),    k(No),       k(Enter),  k(PgUp)],
-        &[k(VolUp), k(LShift),   k(Z),    k(X),    k(C),     k(V),     k(B),     k(N),     k(M),     k(Comma), k(Dot),   k(Slash),  k(LShift),   k(No),       k(Up),     k(PgDown)],
-        &[k(VolDown), k(LCtrl),    k(LGui), k(LAlt), k(No),    k(No),    k(Space), k(No),    k(No),    k(RAlt),  k(RCtrl),  k(RGui),  k(Left),     k(No),       k(Down),   k(Right)],
+        &[k(No),      k(Escape),   k(Kb1),  k(Kb2),  k(Kb3),   k(Kb4),   k(Kb5),   k(Kb6),   k(Kb7),   k(Kb8),   k(Kb9),   k(Kb0),    k(Minus),    k(Equal),    k(BSpace), k(Application)],
+        &[k(LGui),    k(Tab),      k(Q),    k(W),    k(E),     k(R),     k(T),     k(Y),     k(U),     k(I),     k(O),     k(P),      k(LBracket), k(RBracket), k(Bslash), k(Delete)],
+        &[l(1),       k(CapsLock), k(A),    k(S),    k(D),     k(F),     k(G),     k(H),     k(J),     k(K),     k(L),     k(SColon), k(Quote),    k(No),       k(Enter),  k(PgUp)],
+        &[k(Copy),    k(LShift),   k(Z),    k(X),    k(C),     k(V),     k(B),     k(N),     k(M),     k(Comma), k(Dot),   k(Slash),  k(LShift),   k(No),       k(Up),     k(PgDown)],
+        &[k(Paste),   k(LCtrl),    k(LGui), k(LAlt), k(No),    k(No),    k(Space), k(No),    k(No),    k(RAlt),  k(RCtrl),  k(RGui),  k(Left),     k(No),       k(Down),   k(Right)],
+    ], 
+    &[
+        &[k(No),      k(Escape),   k(F1),   k(F2),   k(F3),    k(F4),    k(F5),    k(F6),    k(F7),    k(F8),    k(F9),     k(F10),    k(F11),      k(F12),      k(BSpace), k(Application)],
+        &[k(No),      k(Tab),      k(Q),    k(W),    k(E),     k(R),     k(T),     k(Y),     k(U),     k(I),     k(O),      k(P),      k(LBracket), k(RBracket), k(Bslash), k(Delete)],
+        &[k(Mute),    k(CapsLock), k(A),    k(S),    k(D),     k(F),     k(G),     k(H),     k(J),     k(K),     k(L),      k(SColon), k(Quote),    k(No),       k(Enter),  k(PgUp)],
+        &[k(VolUp),   k(LShift),   k(Z),    k(X),    k(C),     k(V),     k(B),     k(N),     k(M),     k(Comma), k(Dot),    k(Slash),  k(LShift),   k(No),       k(Up),     k(PgDown)],
+        &[k(VolDown), k(LCtrl),    k(LGui), k(LAlt), k(No),    k(No),    k(Space), k(No),    k(No),    k(RAlt),  k(RCtrl),  k(RGui),   k(Left),     k(No),       k(Down),   k(Right)],
     ], 
 ];
